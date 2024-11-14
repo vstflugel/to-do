2ac130bb2306f4ea7e0fb80d1827f8272dd4ac42
Vaibhav Singh Tomar <vst17march@gmail.com>
2024-11-14 17:32:46 +0530

    server.js have been uploaded

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+// import express from "express";
+// import cors from "cors";
+// import records from "./routes/record.js";
+// 
+// const PORT = process.env.PORT || 5050;
+// const app = express();
+// 
+// app.use(cors());
+// app.use(express.json());
+// app.use("", records);
+// 
+// // start the Express server
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+
 import express from "express";
 import cors from "cors";
 import records from "./routes/record.js";
@@ -5,7 +22,11 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors());
+// Allow only your Vercel frontend to access this backend
+app.use(cors({
+  origin: 'https://to-do-frontend-red.vercel.app/' // Replace with the exact URL of your Vercel frontend
+}));
+
 app.use(express.json());
 app.use("", records);
 
@@ -13,3 +34,4 @@ app.use("", records);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
